f7dd184e010de875702f69a77b12e7f7b50076b0
Rodrigo Rosa <rodsiqueirabh@hotmail.com>
2023-11-16 01:57:54 -0300

    Padronizando pacote de testes

--- src/main/kotlin/com/rodao/bankapi/testes/testaComportamentosConta.kt
+++ src/main/kotlin/com/rodao/bankapi/testes/testaComportamentosConta.kt
@@ -4,17 +4,14 @@ import com.rodao.bankapi.models.contas.ContaCorrente
 import com.rodao.bankapi.models.contas.ContaPoupanca
 import com.rodao.bankapi.models.funcionarios.Cliente
 
-class testaComportamentosConta {
+fun testaComportamentosConta() {
+    val numeroX = 10
+    var numeroY = numeroX
+    numeroY++
 
-    fun testaComportamentosConta() {
-        val numeroX = 10
-        var numeroY = numeroX
-        numeroY++
+    println("numeroX $numeroX")
+    println("numeroY $numeroY")
 
-        println("numeroX $numeroX")
-        println("numeroY $numeroY")
-
-        val contaJoao = ContaCorrente(Cliente("João", "123", senha = 123), 10002)
-        val contaMaria = ContaPoupanca(Cliente("Maria", "456", senha = 456), 1003)
-    }
+    val contaJoao = ContaCorrente(Cliente("João", "123", senha = 123), 10002)
+    val contaMaria = ContaPoupanca(Cliente("Maria", "456", senha = 456), 1003)
 }
--- src/main/kotlin/com/rodao/bankapi/testes/testaCopiasEReferencias.kt
+++ src/main/kotlin/com/rodao/bankapi/testes/testaCopiasEReferencias.kt
@@ -4,18 +4,15 @@ import com.rodao.bankapi.models.contas.ContaCorrente
 import com.rodao.bankapi.models.contas.ContaPoupanca
 import com.rodao.bankapi.models.funcionarios.Cliente
 
-class testaCopiasEReferencias {
+fun testaCopiasEReferencias() {
+    val numeroX = 10
+    var numeroY = numeroX
+    numeroY++
 
-    fun testaCopiasEReferencias() {
-        val numeroX = 10
-        var numeroY = numeroX
-        numeroY++
+    println("numeroX $numeroX")
+    println("numeroY $numeroY")
 
-        println("numeroX $numeroX")
-        println("numeroY $numeroY")
-
-        var contaJoao = ContaCorrente(Cliente("João", "123", senha = 123), 10002)
-        contaJoao.titular.nome = "Jones"
-        var contaMaria = ContaPoupanca(Cliente("Maria", "456", senha = 123), 1003)
-    }
+    var contaJoao = ContaCorrente(Cliente("João", "123", senha = 123), 10002)
+    contaJoao.titular.nome = "Jones"
+    var contaMaria = ContaPoupanca(Cliente("Maria", "456", senha = 123), 1003)
 }
